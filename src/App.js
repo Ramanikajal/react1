8c3462434a22ccd00072a35452087ae8818f3059
Ramanikajal <kajaljignesh183@gmail.com>
2022-04-28 10:39:17 +0530

    28/4

--- src/App.js
+++ src/App.js
@@ -2,10 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let arr=[10,20,30,40,];
-  console.log(arr);
+  // let arr=[10,20,30,40,50,60];
+  // console.log(arr[2]);
 
-  arr.map((a,i)=> console.log(a));
+// let  obj={
+//   id:20,
+//   age:30,
+//   per:40,
+
+// }
+// console.log(obj.per);
+
+let arr=[10,20,30,40,50,60];
+//  arr.push(50);
+// arr.unshift(99,56);
+// arr.pop()
+// arr.shift()
+// arr.splice(3,0,78);
+// arr.splice(3,1)
+
+//  console.log(arr. toString());
+// let res=arr.some((a)=>a>100);
+// let res=arr.some((a)=> a > 10);
+// let res= arr.find((a)=>a>10);
+// let res = Array.isarray(arr);
+// let  no = [16, 25, 36];
+// let results = no.map(Math.sqrt);
+// console.log(results);
+// console.log(res);
+
+arr.map((a,i)=> console.log(a));
 
   function intro(){
     console.log("ramani");
