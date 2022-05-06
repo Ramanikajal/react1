de041c82b15b6fb8423da1efc29ac58030dca269
Ramanikajal <kajaljignesh183@gmail.com>
2022-05-06 09:48:21 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -4,71 +4,116 @@ import './App.css';
 function App() {
   let data = [
     {
-      id: 101,
-      name: 'Abacavir',
-      quantity: 25,
-      price: 150,
-      expiry: 2022,
-      status: true
-    },
-    {
-      id: 102,
-      name: 'Eltrombopag',
-      quantity: 90,
-      price: 550,
-      expiry: 2021,
-      status: true
-    },
-    {
+    //   id: 101,
+    //   name: 'Abacavir',
+    //   quantity: 25,
+    //   price: 150,
+    //   expiry: 2022,
+    //   status: true
+    // },
+    // {
+    //   id: 102,
+    //   name: 'Eltrombopag',
+    //   quantity: 90,
+    //   price: 550,
+    //   expiry: 2021,
+    //   status: true
+    // },
+    // {
 
-      id: 103,
-      name: 'Meloxicam',
-      quantity: 85,
-      price: 450,
-      expiry: 2025,
-      status: false
-    },
-    {
-      id: 104,
-      name: 'Allopurinol',
-      quantity: 50,
-      price: 600,
-      expiry: 2023,
+    //   id: 103,
+    //   name: 'Meloxicam',
+    //   quantity: 85,
+    //   price: 450,
+    //   expiry: 2025,
+    //   status: false
+    // },
+    // {
+    //   id: 104,
+    //   name: 'Allopurinol',
+    //   quantity: 50,
+    //   price: 600,
+    //   expiry: 2023,
+    //   status: true
+    // },
+    // {
+    //   id: 105,
+    //   name: 'Phenytoin',
+    //   quantity: 63,
+    //   price: 250,
+    //   expiry: 2021,
+    //   status: false
+    // }
+    
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
       status: true
     },
     {
-      id: 105,
-      name: 'Phenytoin',
-      quantity: 63,
-      price: 250,
-      expiry: 2021,
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
       status: false
-    }];
-  
-  let filterdata=data.filter((v,index)=>v.expiry >= 2022);
-  console.log(filterdata);
-  let ans=filterdata.reduce((acc,d,i)=>acc+d.price,0);
-  console.log(ans);
+    },
+    {
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
+      status: true
+    },
+    {
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
+  ];
+   let filterdata =data.filter((v,i)=> v.status ===true);
+   let totalExpense =filterdata.reduce((ass,v,i)=> ass+v.salary+v.bonus,0);
+  // let filterdata=data.filter((v,index)=>v.expiry >= 2022);
+  // console.log(filterdata);
+  // let ans=filterdata.reduce((acc,d,i)=>acc+d.price,0);
+  // console.log(ans);
 
   //  
   return (
     <>
      <table border = "1"> 
         <tr>
-          <th>id</th>
+        
           <th>name</th>
-          <th>price</th>
-          <th>total</th>
+          <th>age</th>
+          <th>salary</th>
+          <th>bonus</th>
+          <th>total salary</th>
+          <th>totalExpense</th>
+
         </tr>
 
       {
-        filterdata.map(({id,name,price}, index) => {
+        filterdata.map(({name,age,salary,bonus}, index) => {
+          let totalsalary=salary +bonus;
           return (
             <tr>
-              <td> {id}</td>
+             
               <td> {name}</td>
-              <td>{price}</td>
-              {index === 0 ?<td rowspan={filterdata.length}>{ans}</td>:null}
+              <td> {age}</td>
+              <td>{salary}</td>
+              <td> {bonus}</td>
+              <td> {totalsalary}</td>
+              {index === 0 ?<td rowspan={filterdata.length}>{totalExpense}</td>:null}
              </tr>
           )
         })
