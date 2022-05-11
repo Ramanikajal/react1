c3108db4c8a0fb845080a3b4e68bd1d36607333e
Ramanikajal <kajaljignesh183@gmail.com>
2022-05-11 09:54:38 +0530

    function

---
+++ src/containers/propscountry/countryprops.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import React,{useState} from 'react';
+
+function countryprops(props) {
+    const[country,setcountry]=useState('india');
+    const[city,setcity] =useState('haridwar');
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default countryprops;
